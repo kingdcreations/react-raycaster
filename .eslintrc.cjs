df637ec70e1963cd331b869c397158acb4e578e1
kingdcreations <mthais.web@gmail.com>
2024-03-04 15:22:48 +0100

    Fixing module type declarations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,33 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
+  },
+  overrides: [
+    {
+      files: [
+        "*.ts",
+        "*.tsx"
+      ],
+      parserOptions: {
+        project: [
+          "./tsconfig.json"
+        ]
+      }
+    }
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
